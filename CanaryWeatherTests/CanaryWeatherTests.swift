4ff33d2f3a34c29fa9c28f49e46f937827a9fb8b
Tyler Helmrich <tylerhelmrich@tylers-macbook-pro-6.local>
2018-07-24 09:41:25 -0500

    Initial Commit

---
+++ CanaryWeatherTests/CanaryWeatherTests.swift
@@ -0,0 +1,36 @@
+//
+//  CanaryWeatherTests.swift
+//  CanaryWeatherTests
+//
+//  Created by Tyler Helmrich on 7/24/18.
+//  Copyright © 2018 Tyler Helmrich. All rights reserved.
+//
+
+import XCTest
+@testable import CanaryWeather
+
+class CanaryWeatherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
